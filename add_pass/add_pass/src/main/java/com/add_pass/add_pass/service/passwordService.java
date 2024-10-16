dc64f0fff2e7dbd77a1584b59a695967d1b84003
Ayush yadav <113254994+Ayushyadav04@users.noreply.github.com>
2024-10-17 01:22:24 +0530

    Delete add_pass/add_pass directory

--- add_pass/add_pass/src/main/java/com/add_pass/add_pass/service/passwordService.java
+++
@@ -1,7 +0,0 @@
-package com.add_pass.add_pass.service;
-
-import com.add_pass.add_pass.Model.password_list;
-
-public interface passwordService {
-    void postPasswordList(password_list passwordList)throws Exception;
-}
